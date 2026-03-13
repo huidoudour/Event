c8fadfc22c7945be075c937d9402d3f006394d5d
huidoudour <ylmf_qquser@126.com>
2026-03-13 17:11:35 +0800

    03131644-美化UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "me.huidoudour.event"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
